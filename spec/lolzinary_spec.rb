626a10138971ecda4b67531c2fc253635a6a5072
Sam Peters <samantha.peters@fairfaxmedia.com.au>
2015-10-28 15:27:00 +1100

    Removed open-ended dependencies in gemspec

--- spec/lolzinary_spec.rb
+++ spec/lolzinary_spec.rb
@@ -4,8 +4,4 @@ describe Lolzinary do
   it 'has a version number' do
     expect(Lolzinary::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
