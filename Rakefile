d80134c9b4785146e086dee66734a2a06fd8f385
Sam Peters <samantha.peters@fairfaxmedia.com.au>
2015-10-28 15:09:43 +1100

    Makin' the gem work

--- Rakefile
+++ Rakefile
@@ -4,3 +4,10 @@ require "rspec/core/rake_task"
 RSpec::Core::RakeTask.new(:spec)
 
 task :default => :spec
+
+task :console do
+  require 'irb'
+  require 'lolzinary'
+  ARGV.clear
+  IRB.start
+end
--- lib/lolzinary.rb
+++ lib/lolzinary.rb
@@ -1,12 +1,12 @@
 require "lolzinary/version"
+require 'pry'
 
 module Lolzinary
-  def install
-    plugin_directory = "#{FileUtils.pwd}/.lolcommits/.plugins"
-    FileUtils.mkdir plugin_directory unless FileUtils.directory?(plugin_directory)
-    puts "made directory in #{plugin_directory}"
-    puts 'copying files...'
-    FileUtils.cp 'lolzinary.rb', plugin_directory
-    puts "ready!"
+  def self.install
+    source_path = File.expand_path("../lolzinary_plugin.rb", __FILE__)
+    plugin_directory = "~/.lolcommits/.plugins"
+    FileUtils.mkpath plugin_directory
+    FileUtils.cp source_path, plugin_directory
+    puts "Lolzinary plugin installed - remember to enable the plugin with \"lolcommits --config -p lolzinary\""
   end
 end
